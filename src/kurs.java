b33972f7cb1384f8f4cc1a5d5da91619b392e3c2
VSemenov95 <semasvs18@gmail.com>
2021-04-24 16:36:14 +0300

    Итоговая задача №1

--- src/kurs.java
+++ src/kurs.java
@@ -5,15 +5,23 @@ public class kurs {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Курс доллара: ");
-        double d= in.nextDouble();
-        Scanner in1 = new Scanner(System.in);
-        System.out.print("Количество рублей: ");
-        double r= in.nextDouble();
-       //Перевод в доллары
-        double z = r / d;
-        //Задаем формат вывода
-        DecimalFormat df = new DecimalFormat("#0.00");
-        System.out.println("Итого: " + df.format(z) + " долларов");
-
+        double d = in.nextDouble();
+        if (d<=0) {
+            System.out.println("Некорректный ввод");
+        }
+        else {
+            Scanner in1 = new Scanner(System.in);
+            System.out.print("Количество рублей: ");
+            double r = in.nextDouble();
+            if (r >= 0) {
+                //Перевод в доллары
+                double z = r / d;
+                //Задаем формат вывода
+                DecimalFormat df = new DecimalFormat("#0.00");
+                System.out.println("Итого: " + df.format(z) + " долларов");
+            } else {
+                System.out.print("Некорректный ввод");
+            }
+        }
     }
 }
